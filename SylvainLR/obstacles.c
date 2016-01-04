ab099862e0f52d378be5a56f90b5cc99e69ea5c9
Sylvain Le Roux <sylvain.leroux@telecom-paristech.fr>
2016-01-04 21:18:00 +0100

    Ajout d'une marge pour ne pas froler les obstacles de trop près.

--- SylvainLR/obstacles.c
+++ SylvainLR/obstacles.c
@@ -41,16 +41,17 @@ float Obstacle::getHeight() const{
 }
 
 void Obstacle::getCorners(Point out[]) const{
+	float margin = radius/5;
 	//Top Left Corner
-	out[0].x = this->x - (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) - radius;
-	out[0].y = this->y + (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) + radius;
+	out[0].x = this->x - (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) - radius - margin;
+	out[0].y = this->y + (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) + radius + margin;
 	//Top Right Corner
-	out[1].x = this->x + (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) + radius;
-	out[1].y = this->y + (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) + radius;
+	out[1].x = this->x + (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) + radius + margin;
+	out[1].y = this->y + (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) + radius + margin;
 	//Bottom Right Corner
-	out[2].x = this->x + (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) + radius;
-	out[2].y = this->y - (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) - radius;
+	out[2].x = this->x + (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) + radius + margin;
+	out[2].y = this->y - (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) - radius - margin;
 	//Bottom Left Corner
-	out[3].x = this->x - (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) - radius;
-	out[3].y = this->y - (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) - radius;
+	out[3].x = this->x - (this->halfWidth * dcos(-this->angle) + this->halfHeight * dsin(-this->angle)) - radius - margin;
+	out[3].y = this->y - (this->halfHeight * dcos(-this->angle) - this->halfWidth * dsin(-this->angle)) - radius - margin;
 }
