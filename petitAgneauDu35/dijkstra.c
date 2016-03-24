71411589c5c1a6d4d59f2dfc7bdbb1e1ba49f7dc
Sylvain Le Roux <sylvain.leroux@telecom-paristech.fr>
2016-03-24 16:28:04 +0100

    WIP

--- petitAgneauDu35/dijkstra.c
+++ petitAgneauDu35/dijkstra.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
+#define PI 3.14159265
 #define INT_MAX 10000
 
-typedef struct Point {
+typedef struct Node {
 	int accessible;
 	int A;
 	int distance;
 	int x;
 	int y;
-	struct Point* pere;
-} Point;
+	struct Node* pere;
+} Node;
+
+typedef struct Point {
+	int x;
+	int y;
+ } Point;
 
 typedef struct Obstacle {
 	int xPos;
@@ -18,9 +25,15 @@ typedef struct Obstacle {
 	int halfWidth;
 	int halfHeight;
 	int angle;
+	Point topLeft;
+	Point topRight;
+	Point bottomLeft;
+	Point bottomRight;
 } Obstacle;
 
-Point* nextPivot(Point* map[], int xMax, int yMax);
+
+Node* nextPivot(Node* map[], int xMax, int yMax);
+void init_obstacle(Obstacle* obstacle, int xPos, int yPos, int halfWidth, int halfHeight, int angle);
 
 int main(int argc, char* argv[]) {
 	if (argc != 1) {
@@ -40,28 +53,26 @@ int main(int argc, char* argv[]) {
 
 	//Recuperation des coordonnees des obstacles
 	int nbObstacles = 0;
-	int xPos = 1;
 	Obstacle* obstacles = NULL;
-
+	int xPos, yPos, halfWidth, halfHeight, angle;
 	while(scanf("%i[ \n]", &xPos) != EOF) {
 		if (feof(stdin))
 			break;
 		//Allocate space for a new obstacle
 		obstacles = (Obstacle*)realloc(obstacles, (nbObstacles + 1) * sizeof(Obstacle));
 		//Fill the data about the obstacle
-		obstacles[nbObstacles].xPos = xPos;
-		scanf("%i[ \n]", &obstacles[nbObstacles].halfWidth);
-		scanf("%i[ \n]", &obstacles[nbObstacles].yPos);
-		scanf("%i[ \n]", &obstacles[nbObstacles].halfHeight);
-		scanf("%i", &obstacles[nbObstacles].angle);
-
+		scanf("%i[ \n]", &halfWidth);
+		scanf("%i[ \n]", &yPos);
+		scanf("%i[ \n]", &halfHeight);
+		scanf("%i", &.angle);
+		init_obstacle(&obstacles[nbObstacles], xPos, yPos, halfWidth, halfHeight, angle);
 		nbObstacles++;
 	}
 	
-	Point* map[xMax * yMax];
+	Node* map[xMax * yMax];
 	for (int i = 0; i < xMax; i++) {
 		for (int j = 0; j < yMax; j++) {
-			map[i * yMax + j] = (Point*)malloc(sizeof(Point));
+			map[i * yMax + j] = (Node*)malloc(sizeof(Node));
 			map[i * yMax + j]->accessible = 1;
 			map[i * yMax + j]->A = 0;
 			map[i * yMax + j]->distance = INT_MAX;
@@ -75,9 +86,9 @@ int main(int argc, char* argv[]) {
 	map[xStart * yMax + yStart]->distance = 0;
 	//TO DO : inclure les obstacles dans le tableau
 
-	Point* pivot = map[xStart * yMax + yStart];
+	Node* pivot = map[xStart * yMax + yStart];
 	int x, y;
-	Point* cur;
+	Node* cur;
 	for (int i = 0; i < xMax * yMax - 1; i++) {
 		//Pour l'ensemble des voisins de pivot
 		for(int j = -1; j <= 1; j++) {
@@ -104,7 +115,7 @@ int main(int argc, char* argv[]) {
 	//Recuperation du nombre de noeuds sur le chemin
 	x = xStop;
 	y = yStop;
-	Point* pere;
+	Node* pere;
 	int nb = 1;
 	while(x != xStart || y != yStart) {
 		pere = map[x * yMax + y]->pere;
@@ -135,9 +146,9 @@ int main(int argc, char* argv[]) {
 	return 0;
 }
 
-Point* nextPivot(Point* map[], int xMax, int yMax) {
+Node* nextPivot(Node* map[], int xMax, int yMax) {
 	//return the next pivot
-	Point* pivot = (Point*)malloc(sizeof(Point));
+	Node* pivot = (Node*)malloc(sizeof(Node));
 	int freeMemory = 0;
 	pivot->distance = INT_MAX;
 	for (int i = 0; i < xMax * yMax; i++) {
@@ -151,4 +162,39 @@ Point* nextPivot(Point* map[], int xMax, int yMax) {
 		}
 	}
 	return pivot;
+}
+
+int dcos (int angle) {
+	return cos(angle * PI / 180);
+}
+
+int dsin (int angle) {
+	return sin(angle * PI / 180):
+}
+
+void init_obstacle(Obstacle* obstacle, int xPos, int yPos, int halfWidth, int halfHeight, int angle) {
+	obstacle->xPos = xPos;
+	obstacle->yPos = yPos;
+	obstacle->halfWidth = halfWidth;
+	obstacle->halfHeight = halfHeight;
+	obstacle->angle = angle;
+	Point corner;
+	int radius = 0:
+	int margin = 0;
+	//Top Left Corner
+	corner.x = xPos - (halfWidth * dcos(-angle) - halfHeight * dsin(-angle)) - radius - margin;
+	corner.y = yPos + (halfHeight * dcos(-angle) + halfWidth * dsin(-angle)) + radius + margin;
+	obstacle->topLeft = corner;
+	//Top Right Corner
+	corner.x = xPos + (halfWidth * dcos(-angle) + halfHeight * dsin(-angle)) + radius + margin;
+	corner.y = yPos + (halfHeight * dcos(-angle) - halfWidth * dsin(-angle)) + radius + margin;
+	obstacle->topRight = corner;
+	//Bottom Right Corner
+	corner.x = xPos + (halfWidth * dcos(-angle) - halfHeight * dsin(-angle)) + radius + margin;
+	corner.y = yPos - (halfHeight * dcos(-angle) + halfWidth * dsin(-angle)) - radius - margin;
+	obstacle->bottomRight = corner;
+	//Bottom Left Corner
+	corner.x = xPos - (halfWidth * dcos(-angle) + halfHeight * dsin(-angle)) - radius - margin;
+	corner.y = yPos - (halfHeight * dcos(-angle) - halfWidth * dsin(-angle)) - radius - margin;
+	obstacle->bottomLeft = corner;
 }
